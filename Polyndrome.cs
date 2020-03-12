ed79e16fd2f3da627417ff9b5251afda2680802b
babonaresh <nbashaboena@unomaha.edu>
2020-03-12 16:19:04 -0500

    Added Polyndrome

---
+++ Polyndrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Polyndrome
+{
+	public static void Main()
+	{
+		Console.WriteLine(" Enter a number to check if its Polyndrome");
+		var input = int.Parse(Console.ReadLine());
+		int temp, sum = 0, i;
+		for (i = input; i > 1; i++)
+		{
+			temp = i % 10;
+			sum = sum * 10 + temp;
+			i = i / 10;
+		}
+		if (sum == input)
+		{
+			Console.WriteLine("The given number {0} is a Polyndrome", input);
+		}
+		else
+		{
+			Console.WriteLine("The given number {0} is a not a Polyndrome", input);
+		}
+	}
+}
